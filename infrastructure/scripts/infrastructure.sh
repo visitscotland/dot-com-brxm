e0d344bb2f3936e4f792e40ed8756cba8d5aef97
RickONeill <117204466+RickONeill@users.noreply.github.com>
2024-01-18 12:50:23 +0000

    DOPS-1442 FEAT (ci/Jenkinsfile.develop-brc) Testing upload to brcloud

--- infrastructure/scripts/infrastructure.sh
+++ infrastructure/scripts/infrastructure.sh
@@ -940,7 +940,7 @@ case $METHOD in
     # build VS_BRC_API_REMOTE_ARTEFACT to pass to BRC API server
     VS_BRC_API_REMOTE_ARTEFACT="$LOGNAME"@"$VS_HOST_IP_ADDRESS":"$VS_HIPPO_LATEST"
     echo "got: $VS_BRC_API_REMOTE_ARTEFACT"
-    curl -v "https://ci.visitscotland.com/ops/preview/job/visitscotland-multi/job/1_development/job/upload-distribution/buildWithParameters?token=e2c88bc701b075fe7341839a4e9c8914&artefact_remote_location=$VS_BRC_API_REMOTE_ARTEFACT&deploy_after_upload=false" 2>&1 | grep "<" | sed -s 's/^< //'
+    curl -v "https://ci.visitscotland.com/ops/preview/job/visitscotland/job/1_development/job/upload-distribution/buildWithParameters?token=e2c88bc701b075fe7341839a4e9c8914&artefact_remote_location=$VS_BRC_API_REMOTE_ARTEFACT&deploy_after_upload=false" 2>&1 | grep "<" | sed -s 's/^< //'
   ;;
   *)
     echo "`eval $VS_LOG_DATESTAMP` WARN  [$VS_SCRIPTNAME] no function specified - running defaults"
