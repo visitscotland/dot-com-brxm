8c7a1c9d6015bded8b42af434c0cf218b011ce6b
matthew.syrigos <matthew.syrigos@visitscotland.com>
2024-02-14 16:25:11 +0000

    added release start and finish scripts

---
+++ ci/start-release.bat
@@ -0,0 +1,6 @@
+git checkout main
+git pull
+git checkout develop
+git pull
+mvn gitflow:release-start --batch-mode
+mvn versions:use-releases scm:checkin -Dmessage="Updated snapshot dependencies to release versions" -DpushChanges=false
