1214a549fea5452bcc279ce3abfb69989c7a9684
Jose Calcines <46224849+jcalcines@users.noreply.github.com>
2024-02-15 16:51:33 +0000

    Update finish-release.sh

--- ci/finish-release.sh
+++ ci/finish-release.sh
@@ -4,7 +4,6 @@
 branch=$(git branch --list 'release/*' | head -1)
 git stash > /dev/null
 
-
 mvn gitflow:release-finish -DskipTestProject=true
 
 #Recover the workspace to the state previous to run the command
