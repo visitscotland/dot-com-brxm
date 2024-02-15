5f851a1182a8ced38e54024e0434b50d6bf16a63
Jose Calcines <46224849+jcalcines@users.noreply.github.com>
2024-02-15 16:51:23 +0000

    Shelve the workspace before completing the release

--- ci/finish-release.sh
+++ ci/finish-release.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/bash
+
+#Shelve current workspace
+branch=$(git branch --list 'release/*' | head -1)
+git stash > /dev/null
+
+
 mvn gitflow:release-finish -DskipTestProject=true
+
+#Recover the workspace to the state previous to run the command
+git checkout "$branch"
+git stash apply > /dev/null
