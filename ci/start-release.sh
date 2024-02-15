ebe67ee41be44c6a8b47d296ea4e0ce4914d55a6
Jose Calcines <46224849+jcalcines@users.noreply.github.com>
2024-02-15 16:41:11 +0000

    Shelve and Unshelve current workspace

--- ci/start-release.sh
+++ ci/start-release.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
+
+#Shelve current workspace
+branch=$(git branch --show-current)
+git stash > null
+
 git checkout main
 git pull
 git checkout develop
 git pull
 mvn gitflow:release-start --batch-mode
 mvn versions:use-releases scm:checkin -Dmessage="Updated snapshot dependencies to release versions" -DpushChanges=false
+
+#Recover the workspace to the state previous to run the command
+git checkout "$branch"
+git stash apply > null
