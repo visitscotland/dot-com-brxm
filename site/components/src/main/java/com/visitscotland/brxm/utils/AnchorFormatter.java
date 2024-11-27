200a9ddfd610c5eac2b4e289d1e611351662bb6d
Martin.Taylor <martin.taylor@visitscotland.com>
2024-11-27 17:12:22 +0000

    [DS-440] Add `AnchorFormatter.java` Spring Bean, single responsibility is to format anchors

---
+++ site/components/src/main/java/com/visitscotland/brxm/utils/AnchorFormatter.java
@@ -0,0 +1,28 @@
+package com.visitscotland.brxm.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+@Component
+public final class AnchorFormatter {
+    // No Argument Constructor
+    public AnchorFormatter() { }
+
+    public String getAnchorOrFallback(String anchor, Supplier<String> fallback) {
+        if( Objects.isNull(anchor) || StringUtils.isBlank(anchor)) {
+            return formatString(fallback.get());
+        }
+
+        return anchor;
+    }
+
+    private String formatString(String string) {
+        return string
+            .replaceAll("[^a-zA-Z0-9]+", "-")
+            .replaceAll("-+$", "")
+            .toLowerCase();
+    }
+}
---
+++ site/components/src/test/java/com/visitscotland/brxm/services/AnchorFormatterTest.java
@@ -0,0 +1,74 @@
+package com.visitscotland.brxm.services;
+
+import com.visitscotland.brxm.utils.AnchorFormatter;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.function.Supplier;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AnchorFormatterTest {
+    @Mock
+    private Supplier<String> fallbackSupplier;
+    private AnchorFormatter anchorFormatter;
+
+    @BeforeEach
+    void setUp() {
+        this.anchorFormatter = new AnchorFormatter();
+    }
+
+    @Test
+    void testGetAnchorOrFallback_ReturnsAnchor_WhenAnchorIsValid() {
+        final String anchor = "validAnchor";
+
+        final String result = anchorFormatter.getAnchorOrFallback(anchor, () -> "fallbackString");
+
+        Assertions.assertEquals("validAnchor", result, "Should return the provided anchor if valid.");
+    }
+
+    @Test
+    void testGetAnchorOrFallback_UsesFallback_WhenAnchorIsNull() {
+        final String fallback = "fallbackstring";
+
+        when(fallbackSupplier.get()).thenReturn(fallback);
+
+        final String result = anchorFormatter.getAnchorOrFallback(null, fallbackSupplier);
+
+        Assertions.assertEquals("fallbackstring", result);
+        verify(fallbackSupplier, times(1)).get();
+    }
+
+    @Test
+    void testGetAnchorOrFallback_UsesFallback_WhenAnchorIsBlank() {
+        final String fallback = "fallbackstring";
+
+        when(fallbackSupplier.get()).thenReturn(fallback);
+
+        final String result = anchorFormatter.getAnchorOrFallback("   ", fallbackSupplier);
+
+        Assertions.assertEquals("fallbackstring", result);
+        verify(fallbackSupplier, times(1)).get();
+    }
+
+    @Test
+    void testGetAnchorOrFallback_FormatsFallbackCorrectly() {
+        // Arrange
+        final String fallback = "Fallback String with !@#Symbols ?";
+
+        when(fallbackSupplier.get()).thenReturn(fallback);
+
+        final String result = anchorFormatter.getAnchorOrFallback(null, fallbackSupplier);
+
+        // Assert
+        Assertions.assertEquals("fallback-string-with-symbols", result);
+    }
+
+}
