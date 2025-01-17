bd6ab351f25e525227ce464a482d21d9703fc572
Martin.Taylor <martin.taylor@visitscotland.com>
2025-01-17 12:05:17 +0000

    DS-859-REFACTOR Create initial package and package-info.java

---
+++ site/components/src/main/java/com/visitscotland/brxm/jcr/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides classes and interfaces for interacting with the Java Content Repository (JCR)
+ * within the VisitScotland application scope. This package contains functionality for
+ * querying, iterating, and searching through JCR nodes and content structures.
+ * 
+ * The classes in this package facilitate access and manipulation of content stored in
+ * the JCR, supporting VisitScotland's content management requirements.
+ */
+package com.visitscotland.brxm.jcr;
