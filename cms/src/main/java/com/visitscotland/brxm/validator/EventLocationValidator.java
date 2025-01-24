abc089e263e5b6540403cc473674c97fdd6b36f4
Martin.Taylor <martin.taylor@visitscotland.com>
2025-01-24 15:17:22 +0000

    DS-902-VALIDATOR Add initial logic to validator

--- cms/src/main/java/com/visitscotland/brxm/validator/EventLocationValidator.java
+++ cms/src/main/java/com/visitscotland/brxm/validator/EventLocationValidator.java
@@ -1,4 +1,36 @@
 package com.visitscotland.brxm.validator;
 
-class EventLocationValidator {
+import org.onehippo.cms.services.validation.api.ValidationContext;
+import org.onehippo.cms.services.validation.api.Validator;
+import org.onehippo.cms.services.validation.api.Violation;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import java.util.Optional;
+
+final class EventLocationValidator implements Validator<Node> {
+    private static final String VENUE_JCR_TYPE = "visitscotland:venue";
+    private static final String IS_EVENT_ONLINE_JCR_TYPE = "visitscotland:online";
+
+    @Override
+    public Optional<Violation> validate(ValidationContext validationContext, Node node) {
+        final boolean isValid = isNodeValid(node);
+        return isValid ? Optional.empty() : Optional.of(validationContext.createViolation());
+    }
+
+    private boolean isNodeValid(final Node node) {
+        try {
+            return isEventOnline(node) || !isVenueBlank(node);
+        } catch (RepositoryException e) {
+            return false;
+        }
+    }
+
+    private boolean isEventOnline(final Node node) throws RepositoryException {
+        return node.getProperty(IS_EVENT_ONLINE_JCR_TYPE).getBoolean();
+    }
+
+    private boolean isVenueBlank(final Node node) throws RepositoryException {
+        return node.getProperty(VENUE_JCR_TYPE).getString().isBlank();
+    }
 }
