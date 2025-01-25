962db111a86bb70252eaa58f8e8056784845af89
jose.calcines <jcalcines@gmail.com>
2025-01-25 01:09:14 +0000

    DS-908 - Create basic UI module

---
+++ site/components/src/main/java/com/visitscotland/brxm/model/EventsLingsModule.java
@@ -0,0 +1,11 @@
+package com.visitscotland.brxm.model;
+
+import com.visitscotland.brxm.hippobeans.EventsListing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventsLingsModule extends Module<EventsListing> {
+
+    private final List<EventsListingTab> eventsListings = new ArrayList<>();
+}
