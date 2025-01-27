84d9967cbf578104f768a779cb15f309924d66e7
jose.calcines <jcalcines@gmail.com>
2025-01-27 19:00:21 +0000

    DS-964 - Allow Assets to be linked from the CMS

--- site/components/src/main/java/com/visitscotland/brxm/hippobeans/FileLink.java
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/FileLink.java
@@ -4,12 +4,22 @@ import com.visitscotland.brxm.hippobeans.capabilities.UrlLink;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 
 @HippoEssentialsGenerated(internalName = "visitscotland:FileLink")
 @Node(jcrType = "visitscotland:FileLink")
 public class FileLink extends HippoCompound implements UrlLink {
+
+    public static final String LINK = "visitscotland:link";
+    public static final String ASSET = "visitscotland:asset";
+
     @HippoEssentialsGenerated(internalName = "visitscotland:link")
     public String getLink() {
         return getSingleProperty("visitscotland:link");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:asset")
+    public HippoBean getAsset() {
+        return getLinkedBean("visitscotland:asset", HippoBean.class);
     }
 }
