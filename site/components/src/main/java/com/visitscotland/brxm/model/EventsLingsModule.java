57bfa50b877d0a6754d3d402f09bbdaa48be916c
jose.calcines <jcalcines@gmail.com>
2025-01-31 23:27:02 +0000

    DS-908 - Define the models for the filters

--- site/components/src/main/java/com/visitscotland/brxm/model/EventsLingsModule.java
+++ site/components/src/main/java/com/visitscotland/brxm/model/EventsLingsModule.java
@@ -1,6 +1,7 @@
 package com.visitscotland.brxm.model;
 
 import com.visitscotland.brxm.hippobeans.EventsListing;
+import com.visitscotland.brxm.model.event.EventsListingTab;
 
 import java.util.ArrayList;
 import java.util.List;
