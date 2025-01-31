8d96b751fd71f95f4af3a8d7b497946fffa68173
Martin.Taylor <martin.taylor@visitscotland.com>
2025-01-31 15:19:05 +0000

    BSHUB-525 Tidy unit tests

--- site/components/src/test/java/com/visitscotland/brxm/event/PriceFormatterTest.java
+++ site/components/src/test/java/com/visitscotland/brxm/event/PriceFormatterTest.java
@@ -5,19 +5,21 @@ import com.visitscotland.brxm.hippobeans.Price;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import java.util.Locale;
 
 import static org.mockito.ArgumentMatchers.any;
+
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.eq;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,22 +29,16 @@ class PriceFormatterTest {
     private static final String EVENT_VAT_LABEL_KEY = "price.vat";
     private static final String ISO_4217_UK_CURRENCY_CODE = "GBP";
 
-    private PriceFormatter priceFormatter;
-
-    @Mock
-    private Price price;
-
     @Mock
     private ResourceBundleService resourceBundleService;
 
-    @BeforeEach
-    void setUp() {
-        this.priceFormatter = new PriceFormatter(resourceBundleService);
-    }
+    @InjectMocks
+    private PriceFormatter priceFormatter;
 
     @Test
     void formatPrice_ValidPriceNoVat_ExpectedFormat() {
         final String expected = "1470.32 GBP";
+        final Price price = mock(Price.class);
 
         when(price.getPrice()).thenReturn(1470.32D);
         when(price.getCurrency()).thenReturn(ISO_4217_UK_CURRENCY_CODE);
@@ -59,6 +55,7 @@ class PriceFormatterTest {
     void formatPrice_ValidPriceWithVat_ExpectedFormat() {
         final String vatLabel = "+VAT";
         final String expected = "10.00 GBP " + vatLabel;
+        final Price price = mock(Price.class);
 
         when(price.getPrice()).thenReturn(10.00D);
         when(price.getCurrency()).thenReturn(ISO_4217_UK_CURRENCY_CODE);
@@ -77,6 +74,7 @@ class PriceFormatterTest {
     @Test
     void formatPrice_PriceNull_ExpectNull() {
         final String result = priceFormatter.format(null);
+        final Price price = mock(Price.class);
 
         Assertions.assertNull(result);
 
@@ -88,6 +86,7 @@ class PriceFormatterTest {
     @Test
     void formatPrice_PriceZero_ExpectFree() {
         final String expected = "free";
+        final Price price = mock(Price.class);
 
         when(price.getPrice()).thenReturn(0.00D);
         when(price.getCurrency()).thenReturn(ISO_4217_UK_CURRENCY_CODE);
