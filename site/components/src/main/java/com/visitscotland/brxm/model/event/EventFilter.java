613fdb745639dc1dfb9e5ece96ef422f081c9515
jose.calcines <jcalcines@gmail.com>
2025-01-31 23:27:02 +0000

    DS-908 - Define the models for the filters

---
+++ site/components/src/main/java/com/visitscotland/brxm/model/event/EventFilter.java
@@ -0,0 +1,56 @@
+package com.visitscotland.brxm.model.event;
+
+import java.util.List;
+
+public class EventFilter {
+
+    public enum Type {
+        BOOLEAN, DATE, MULTI_SELECT, DROPDOWN
+    }
+
+    private String key;
+    private String label;
+    private Type type;
+    private Integer group;
+    private List<EventValueOption> values;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Integer getGroup() {
+        return group;
+    }
+
+    public void setGroup(Integer group) {
+        this.group = group;
+    }
+
+    public List<EventValueOption> getValues() {
+        return values;
+    }
+
+    public void setValues(List<EventValueOption> values) {
+        this.values = values;
+    }
+}
