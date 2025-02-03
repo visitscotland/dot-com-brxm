c1ca59086d0c6c43999d502516bd204b2c4a1c05
Martin.Taylor <martin.taylor@visitscotland.com>
2025-02-03 12:46:49 +0000

    BSHUB-518 - Add validation logic and unit tests

---
+++ cms/src/main/java/com/visitscotland/brxm/validator/EventPriceValidator.java
@@ -0,0 +1,42 @@
+package com.visitscotland.brxm.validator;
+
+import org.onehippo.cms.services.validation.api.ValidationContext;
+import org.onehippo.cms.services.validation.api.Validator;
+import org.onehippo.cms.services.validation.api.Violation;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import java.util.Optional;
+
+public class EventPriceValidator implements Validator<Node> {
+    public EventPriceValidator() { }
+
+    @Override
+    public Optional<Violation> validate(ValidationContext context, Node node) {
+        try {
+            if(isPriceInvalid(node)) {
+                return Optional.of(context.createViolation());
+            }
+        } catch (RepositoryException exception) {
+            return Optional.of(context.createViolation("exception"));
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Checks if a price is valid
+     * @param node The JCR node to check
+     * @return true if the price is invalid, false otherwise
+     * @throws RepositoryException if there is an error accessing the JCR node
+     */
+    private boolean isPriceInvalid(final Node node) throws RepositoryException {
+        if(node.hasProperty("visitscotland:price")) {
+            final double price = node.getProperty("visitscotland:price").getDouble();
+            return Double.isNaN(price) || price < 0;
+        }
+
+       return false;
+    }
+}
--- cms/src/test/java/com/visitscotland/brxm/validator/EventPriceValidatorTest.java
+++ cms/src/test/java/com/visitscotland/brxm/validator/EventPriceValidatorTest.java
@@ -1,27 +1,84 @@
 package com.visitscotland.brxm.validator;
 
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.onehippo.cms.services.validation.api.ValidationContext;
+import org.onehippo.cms.services.validation.api.Violation;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class EventPriceValidatorTest {
-    private static final String ISO_4217_UK_CURRENCY_CODE = "GBP";
+    private static final String PRICE_JCR_PROPERTY_NAME = "visitscotland:price";
+    private static final String EXCEPTION_TRANSLATION_VARIATION = "exception";
+    private static final double NEGATIVE_DOUBLE_VALUE = -17.99D;
+    private static final double POSITIVE_DOUBLE_VALUE = 17.99D;
+    private static final double ZERO_DOUBLE_VALUE = 0.00D;
 
-    void validate_PriceNodeWithValidCurrencyPositivePriceAndVatTrue_OptionalEmpty() { }
-    void validate_PriceNodeWithInvalidCurrencyPositivePriceAndVatTrue_OptionalOfViolation() { }
-    void validate_PriceNodeWithValidCurrencyAndInvalidPrice_OptionalOfViolation() { }
-    void validate_nullPriceNode_OptionalOfExceptionalViolation() { }
-    void validate_nullCurrencyProperty_OptionalOfExceptionalViolation() { }
-    void validate_nullPriceProperty_OptionalOfExceptionalViolation() { }
-    void validate_PriceNodeWithValidCurrencyZeroPriceAndVatTrue_OptionalEmpty() { }
-    void validate_PriceNodeWithValidCurrencyBlankPriceAndVatTrue_OptionalEmpty() { }
-    void validate_PriceNodeWithValidCurrencyPositivePriceAndInvalidVat_OptionalOfViolation() { }
-    void validate_PriceNodeWithNonIso4217CurrencyPositivePriceAndVatTrue_OptionalOfViolation() { }
-    void validate_PriceNodeWithEmptyCurrencyPositivePriceAndVatTrue_OptionalOfViolation() { }
+    @Mock private Node node;
+    @Mock private Property property;
+    @Mock private ValidationContext validationContext;
+
+    private EventPriceValidator eventPriceValidator;
+
+    @BeforeEach
+    void setUp() {
+        this.eventPriceValidator = new EventPriceValidator();
+    }
+
+    @ValueSource(doubles = { POSITIVE_DOUBLE_VALUE, ZERO_DOUBLE_VALUE })
+    @ParameterizedTest
+    void validate_PriceNodeWithPositivePrice_OptionalEmpty(double value) throws RepositoryException {
+        when(node.hasProperty(PRICE_JCR_PROPERTY_NAME)).thenReturn(true);
+        when(node.getProperty(PRICE_JCR_PROPERTY_NAME)).thenReturn(property);
+        when(property.getDouble()).thenReturn(value);
+
+        final Optional<Violation> result = eventPriceValidator.validate(validationContext, node);
+
+        Assertions.assertTrue(result.isEmpty());
+
+        verify(node, times(1)).hasProperty(eq(PRICE_JCR_PROPERTY_NAME));
+        verify(node, times(1)).getProperty(eq(PRICE_JCR_PROPERTY_NAME));
+        verify(property, times(1)).getDouble();
+    }
+
+    @Test
+    void validate_PriceNodeWithNegativePrice_OptionalOfViolation() throws RepositoryException {
+        when(node.hasProperty(PRICE_JCR_PROPERTY_NAME)).thenReturn(true);
+        when(node.getProperty(PRICE_JCR_PROPERTY_NAME)).thenReturn(property);
+        when(property.getDouble()).thenReturn(NEGATIVE_DOUBLE_VALUE);
+        when(validationContext.createViolation()).thenReturn(mock(Violation.class));
+
+        final Optional<Violation> result = eventPriceValidator.validate(validationContext, node);
+
+        Assertions.assertTrue(result.isPresent());
+
+        verify(validationContext, times(1)).createViolation();
+    }
+
+    @Test
+    void validate_PriceNodeGetPricePropertyThrowsRepositoryException_OptionalOfExceptionalViolation() throws RepositoryException {
+        final Exception repositoryException = new RepositoryException();
+
+        when(node.hasProperty(PRICE_JCR_PROPERTY_NAME)).thenReturn(true);
+        when(node.getProperty(PRICE_JCR_PROPERTY_NAME)).thenThrow(repositoryException);
+        when(validationContext.createViolation(EXCEPTION_TRANSLATION_VARIATION)).thenReturn(mock(Violation.class));
+
+        final Optional<Violation> result = eventPriceValidator.validate(validationContext, node);
+
+        Assertions.assertTrue(result.isPresent());
+
+        verify(validationContext, times(1)).createViolation(eq(EXCEPTION_TRANSLATION_VARIATION));
+    }
 }
