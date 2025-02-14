07955ae380fb915d4592fd9df6cda11aee43af1d
Martin.Taylor <martin.taylor@visitscotland.com>
2025-02-14 16:21:30 +0000

    BSHUB-1025 - Rename formatter to converter for clarity.

--- site/components/src/main/java/com/visitscotland/brxm/formatter/HippoBeanFormatter.java
+++
@@ -1,8 +0,0 @@
-package com.visitscotland.brxm.formatter;
-
-import org.hippoecm.hst.content.beans.standard.HippoBean;
-
-@FunctionalInterface
-public interface HippoBeanFormatter<H extends HippoBean, R> {
-    R format(H hippoBean);
-}
