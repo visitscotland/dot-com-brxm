4e5b8825116a550ca51b029c73e83b2d21c934db
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2025-03-13 10:39:49 +0000

    FEAT BSHUB-429 (Download) validator

---
+++ cms/src/main/java/com/visitscotland/brxm/validator/MegaLinksBSHValidator.java
@@ -0,0 +1,69 @@
+package com.visitscotland.brxm.validator;
+
+import org.onehippo.cms.services.validation.api.ValidationContext;
+import org.onehippo.cms.services.validation.api.Validator;
+import org.onehippo.cms.services.validation.api.Violation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import java.util.Optional;
+
+public class MegaLinksBSHValidator implements Validator<Node> {
+    private static final Logger logger = LoggerFactory.getLogger(MultiValueNumberSelectedValidator.class);
+
+    private static final String MAX_LENGTH = "maxLength";
+    private static final String TARGET_FIELD = "targetField";
+
+    private Long maxLength;
+    private String targetField;
+
+    @Override
+    public Optional<Violation> validate(ValidationContext context, Node node) {
+        try {
+            targetField = node.getProperty(TARGET_FIELD).getString();
+            maxLength = node.getProperty(MAX_LENGTH).getLong();
+            if(isPriceInvalid(node)) {
+                return Optional.of(context.createViolation());
+            }
+        } catch (RepositoryException exception) {
+            return Optional.of(context.createViolation("exception"));
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Checks if a price is valid
+     * @param node The JCR node to check
+     * @return true if the price is invalid, false otherwise
+     * @throws RepositoryException if there is an error accessing the JCR node
+     */
+    private boolean isPriceInvalid(final Node node) throws RepositoryException {
+        if(node.getProperty("visitscotland:layout").getValue().getString().equals("Download")) {
+            return countNumberOfItems(node) < 2;
+        }
+
+        return true;
+    }
+
+    private Integer countNumberOfItems(Node node) {
+        try {
+            if (node.hasNode(targetField)) {
+                int count = 0;
+                NodeIterator childNodes = node.getNodes(targetField);
+                while (childNodes.hasNext()) {
+                    count++;
+                    childNodes.nextNode();
+                }
+                return count;
+            }
+            logger.error("Can not run MultiValueNumberSelectedValidator as node {} has no property or child {}", node.getPath(), targetField);
+        } catch (RepositoryException ex) {
+            logger.error("Repository error when running multi value validator", ex);
+        }
+        return 0;
+    }
+}
