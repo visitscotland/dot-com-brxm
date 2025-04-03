d4d449deba2dc93d0bb4ab2c52cd5995d69b1b0e
jose.calcines <jcalcines@gmail.com>
2025-04-03 23:01:50 +0100

    BSHUB-753 - Inlcude SEO configuration in the ResourceAPI

--- site/components/src/main/java/com/visitscotland/brxm/components/content/PageContentComponent.java
+++ site/components/src/main/java/com/visitscotland/brxm/components/content/PageContentComponent.java
@@ -132,11 +132,13 @@ public class PageContentComponent<T extends Page> extends ContentComponent {
         final String SKIP_TO = "skip-to";
         final String SEARCH_BUNDLE = "search";
         final String CMS_MESSAGES = "cms-messages";
+        final String SEO = "seo";
 
         labels(request).put(ResourceBundleService.GLOBAL_BUNDLE_FILE, getGlobalLabels(request.getLocale()));
         labels(request).put(SOCIAL_SHARE_BUNDLE, bundle.getAllLabels(SOCIAL_SHARE_BUNDLE, request.getLocale()));
         labels(request).put(SEARCH_BUNDLE, bundle.getAllLabels(SEARCH_BUNDLE, request.getLocale()));
         labels(request).put(VIDEO_BUNDLE, bundle.getAllLabels(VIDEO_BUNDLE, request.getLocale()));
+        labels(request).put(SEO, bundle.getAllSiteLabels(SEO, request.getLocale()));
         labels(request).put(SKIP_TO, bundle.getAllLabels(SKIP_TO, request.getLocale()));
 
         if (isEditMode(request)) {
