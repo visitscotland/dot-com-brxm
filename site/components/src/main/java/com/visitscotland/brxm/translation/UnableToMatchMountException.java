ca761c6733dbaa6dde13da0af54b30a46d0ca3a1
Martin.Taylor <martin.taylor@visitscotland.com>
2025-04-03 13:44:54 +0100

    DS-1185 - Address PR Code Rabbit comment

--- site/components/src/main/java/com/visitscotland/brxm/translation/UnableToMatchMountException.java
+++ site/components/src/main/java/com/visitscotland/brxm/translation/UnableToMatchMountException.java
@@ -6,7 +6,7 @@ final class UnableToMatchMountException extends TranslationException {
     private static final String EXCEPTION_MESSAGE =
         "failed to resolve request path '%s' to a mount that is on the virtual host %s";
 
-    public UnableToMatchMountException(final String requestPath,
+    UnableToMatchMountException(final String requestPath,
                                        final ResolvedVirtualHost resolvedVirtualHost) {
         super(String.format(EXCEPTION_MESSAGE, requestPath, resolvedVirtualHost.getVirtualHost().getName()));
     }
