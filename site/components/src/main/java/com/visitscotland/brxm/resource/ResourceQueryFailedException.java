1da7245c13a63a198db505a3e4a0f03936f23950
Martin.Taylor <martin.taylor@visitscotland.com>
2025-04-17 12:21:29 +0100

    DS-1226 Restricted lenient class visibility

--- site/components/src/main/java/com/visitscotland/brxm/resource/ResourceQueryFailedException.java
+++ site/components/src/main/java/com/visitscotland/brxm/resource/ResourceQueryFailedException.java
@@ -1,10 +1,10 @@
 package com.visitscotland.brxm.resource;
 
-public class ResourceQueryFailedException extends ResourceException {
+class ResourceQueryFailedException extends ResourceException {
     private static final String EXCEPTION_MESSAGE =
         "Attempt to query the requested resource bundle, %s, has failed";
 
-    public ResourceQueryFailedException(final String bundleName, final Throwable cause) {
+    ResourceQueryFailedException(final String bundleName, final Throwable cause) {
         super(String.format(EXCEPTION_MESSAGE, bundleName), cause);
     }
 }
