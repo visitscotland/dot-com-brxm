d98b4711c3988ffd8294d84d71babfd79af17a3d
Martin.Taylor <martin.taylor@visitscotland.com>
2025-04-18 14:47:21 +0100

    DS-1226 Enhance java doc for resource base exception

--- site/components/src/main/java/com/visitscotland/brxm/resource/ResourceException.java
+++ site/components/src/main/java/com/visitscotland/brxm/resource/ResourceException.java
@@ -1,5 +1,12 @@
 package com.visitscotland.brxm.resource;
 
+/**
+ * Base exception class for all resource-related errors in the application.
+ * <p>
+ * This exception serves as the superclass for more specific resource exceptions,
+ * providing a consistent way to represent errors encountered during resource handling.
+ * </p>
+ */
 public abstract class ResourceException extends RuntimeException {
     protected ResourceException(String message, Throwable cause) {
         super(message, cause);
