2c13325b53a54c528d733486305593a2dbfa10d9
jose.calcines <jcalcines@gmail.com>
2025-05-09 16:29:01 +0100

    DS-1261: Break the class in 2 parts to increase testability

--- site/components/src/main/java/com/visitscotland/brxm/utils/EnvironmentManager.java
+++ site/components/src/main/java/com/visitscotland/brxm/utils/EnvironmentManager.java
@@ -1,6 +1,9 @@
 package com.visitscotland.brxm.utils;
 
+import com.visitscotland.brxm.dms.DMSProxy;
 import com.visitscotland.utils.Contract;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -8,26 +11,35 @@ import java.util.Optional;
 @Component
 public class EnvironmentManager {
 
-    @NonTestable(value = NonTestable.Cause.WRAP)
+    private static final Logger logger = LoggerFactory.getLogger(DMSProxy.class);
+
+    final SystemWrapper system;
+
+    public EnvironmentManager(final SystemWrapper system) {
+        this.system = system;
+    }
+
+
     Optional<String> getEnvironmentVariable(String name) {
         try {
-            var value = System.getenv(name);
+            var value = system.getenv(name);
             if (!Contract.isEmpty(value)){
                 return Optional.of(value);
             }
-        } catch (RuntimeException e){
-            // If the variable does not exist or cannot be accessed the error is ignored and an empty value is returned
+        } catch (NullPointerException | SecurityException e){
+            logger.debug("The environment Variable {} could not be read due to the following error: {}", name, e.getMessage());
         }
         return Optional.empty();
     }
 
     Optional<String> getSystemProperty(String name){
         try {
-            var value = System.getProperty(name, "");
+            var value = system.getProperty(name);
             if (!Contract.isEmpty(value)){
                 return Optional.of(value);
             }
-        } catch (RuntimeException e){
+        } catch (NullPointerException | SecurityException e){
+            logger.debug("The system property {} could not be read due to the following error: {}", name, e.getMessage());
             // If the variable does not exist or cannot be accessed the error is ignored and an empty value is returned
         }
         return Optional.empty();
