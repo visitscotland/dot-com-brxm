66c21c8582f06fa16ae280fd504241eca5ea0028
jose.calcines <jose.calcines@visitscotland.com>
2025-06-03 16:08:58 +0100

    DS-801 Create EntryMapper

---
+++ site/components/src/main/java/com/visitscotland/brxm/factory/EntryMapper.java
@@ -0,0 +1,30 @@
+package com.visitscotland.brxm.factory;
+
+import com.visitscotland.brxm.factory.hippo.ValueList;
+import com.visitscotland.brxm.model.megalinks.Entry;
+import com.visitscotland.brxm.services.HippoUtilsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+class EntryMapper {
+
+    private static final Logger logger = LoggerFactory.getLogger(EntryMapper.class);
+
+    private final HippoUtilsService hippoUtils;
+
+    public EntryMapper(HippoUtilsService hippoUtils) {
+        this.hippoUtils = hippoUtils;
+    }
+
+    Entry getEntry(String key, ValueList valueList) {
+        String displayText = hippoUtils.getValueMap(valueList.id()).get(key);
+        if (displayText == null) {
+            logger.warn("No display text found for the key '{}' in the Value List '{}'", key, valueList.id());
+            displayText = key; // Fallback to using the key as display text
+        }
+        return new Entry(key, displayText);
+    }
+
+}
