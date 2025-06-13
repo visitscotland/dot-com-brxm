bc807b4d8a4beaed5509e8daea0d2d69b2ba1ab2
jose.calcines <jose.calcines@visitscotland.com>
2025-06-13 08:42:17 +0100

    BSHUB-883 - Generate Models

---
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/ObsProvider.java
@@ -0,0 +1,33 @@
+package com.visitscotland.brxm.hippobeans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "visitscotland:ObsProvider")
+@Node(jcrType = "visitscotland:ObsProvider")
+public class ObsProvider extends BaseDocument {
+    @HippoEssentialsGenerated(internalName = "visitscotland:name")
+    public String getName() {
+        return getSingleProperty("visitscotland:name");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:url")
+    public String getUrl() {
+        return getSingleProperty("visitscotland:url");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:functions")
+    public String[] getFunctions() {
+        return getMultipleProperty("visitscotland:functions");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:contract")
+    public String[] getContract() {
+        return getMultipleProperty("visitscotland:contract");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:contractLegth")
+    public String getContractLegth() {
+        return getSingleProperty("visitscotland:contractLegth");
+    }
+}
