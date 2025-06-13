17bf0a5e996409c198b6e6299053156279cd63e7
jose.calcines <jose.calcines@visitscotland.com>
2025-06-13 12:25:16 +0100

    BSHUB-1356 - Add description field

--- site/components/src/main/java/com/visitscotland/brxm/obs/model/Provider.java
+++ site/components/src/main/java/com/visitscotland/brxm/obs/model/Provider.java
@@ -2,6 +2,7 @@ package com.visitscotland.brxm.obs.model;
 
 import com.visitscotland.brxm.hippobeans.ObsProvider;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
 
 //TODO: Convert to record
 public class Provider {
@@ -10,6 +11,8 @@ public class Provider {
     private String[] functions;
     private String[] contract;
     private String contractLength;
+    private String description;
+
 
     public Provider(ObsProvider hippoBean) {
         this.name = hippoBean.getName();
@@ -17,46 +20,30 @@ public class Provider {
         this.functions = hippoBean.getFunctions();
         this.contract = hippoBean.getContract();
         this.contractLength = hippoBean.getContractLegth();
-
+        this.description = hippoBean.getDescription().getContent();
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getUrl() {
         return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 
     public String[] getFunctions() {
         return functions;
     }
 
-    public void setFunctions(String[] functions) {
-        this.functions = functions;
-    }
-
     public String[] getContract() {
         return contract;
-    }
-
-    public void setContract(String[] contract) {
-        this.contract = contract;
     }
 
     public String getContractLength() {
         return contractLength;
     }
 
-    public void setContractLength(String contractLength) {
-        this.contractLength = contractLength;
+    public String getDescription() {
+        return description;
     }
 }
