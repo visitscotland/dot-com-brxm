a050e620249764fbe45630e751c4804ff0f04435
jose.calcines <jose.calcines@visitscotland.com>
2025-06-13 15:53:49 +0100

    BSHUB-1356 - Fix Bug

--- site/components/src/main/java/com/visitscotland/brxm/obs/ObsFormRestService.java
+++ site/components/src/main/java/com/visitscotland/brxm/obs/ObsFormRestService.java
@@ -4,10 +4,12 @@ import com.visitscotland.brxm.obs.form.Form;
 import com.visitscotland.brxm.obs.model.FormBuilder;
 import com.visitscotland.brxm.obs.model.Function;
 import com.visitscotland.brxm.utils.VsException;
+import com.visitscotland.utils.Contract;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.jaxrs.services.AbstractResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
@@ -18,7 +20,7 @@ import javax.ws.rs.core.Response;
  *
  * https://en.wikipedia.org/wiki/Jakarta_RESTful_Web_Services#Implementations
  */
-@Path("/obs/")
+@Path("/obs/form")
 public class ObsFormRestService extends AbstractResource {
 
     private static final Logger logger = LoggerFactory.getLogger(ObsFormRestService.class);
@@ -31,18 +33,30 @@ public class ObsFormRestService extends AbstractResource {
     }
 
     @GET
-    @Path("/form")
+    @Path("/json-config")
     @Produces("application/json")
     public Response list(@Context HstRequest request,
-                             @DefaultValue("hst:root") @QueryParam("channel") String locale) {
+                         @DefaultValue("") @QueryParam("category") String category) {
         try {
-            return Response.ok().entity(buildForm()).build();
+            return Response.ok().entity(buildForm(category)).build();
         } catch (VsException e){
             return Response.serverError().build();
         }
     }
 
-    private Form buildForm() {
+    @POST
+    @Path("/submit")
+    @Produces("application/json")
+    public Response submit(@Context HstRequest request,
+                           @RequestBody String body) {
+        try {
+            return Response.ok().entity(body).build();
+        } catch (VsException e){
+            return Response.serverError().build();
+        }
+    }
+
+    private Form buildForm(String category) {
         FormBuilder builder = FormBuilder.create()
                 .input("Business Name", true)
                 .email("Email Address", true)
@@ -51,9 +65,20 @@ public class ObsFormRestService extends AbstractResource {
                 .input("Location", false);
 
         for (Function f : new ComparisonMapper().getFunctions()) {
-            builder.selectImportance(f.getDescription(), f.getId(), true);
+            if (Contract.isEmpty(category) || in(category.toLowerCase(), f.getCategory())){
+                builder.selectImportance(f.getDescription(), f.getId(), true);
+            }
         }
 
         return builder.build();
+    }
+
+    private boolean in(String value, String[] list){
+        for (String elm : list) {
+            if (value.trim().equalsIgnoreCase(elm)){
+                return true;
+            }
+        }
+        return false;
     }
 }
