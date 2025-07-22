b7f4e52afb347152875c9a3d0ebf0ae7e1cc0f1f
jose.calcines <jose.calcines@visitscotland.com>
2025-07-22 13:30:03 +0100

    DS-1433 - Generate Beans for Comparator System

---
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/ComparatorSystem.java
@@ -0,0 +1,29 @@
+package com.visitscotland.brxm.hippobeans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+
+@HippoEssentialsGenerated(internalName = "visitscotland:ComparatorSystem")
+@Node(jcrType = "visitscotland:ComparatorSystem")
+public class ComparatorSystem extends BaseDocument {
+    @HippoEssentialsGenerated(internalName = "visitscotland:name")
+    public String getName() {
+        return getSingleProperty("visitscotland:name");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:url")
+    public String getUrl() {
+        return getSingleProperty("visitscotland:url");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:features")
+    public String[] getFeatures() {
+        return getMultipleProperty("visitscotland:features");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:description")
+    public HippoHtml getDescription() {
+        return getHippoHtml("visitscotland:description");
+    }
+}
