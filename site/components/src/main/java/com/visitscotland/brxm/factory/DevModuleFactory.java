ca235f21d72fd8b0c000535e6605f5d22123787a
jose.calcines <jose.calcines@visitscotland.com>
2025-07-23 16:41:41 +0100

    DS-1433 - Allow Dev module to include bespoke UI components

--- site/components/src/main/java/com/visitscotland/brxm/factory/DevModuleFactory.java
+++ site/components/src/main/java/com/visitscotland/brxm/factory/DevModuleFactory.java
@@ -1,13 +1,36 @@
 package com.visitscotland.brxm.factory;
 
+import com.visitscotland.brxm.comparator.ComparisonMapper;
+import com.visitscotland.brxm.comparator.ResultMapper;
+import com.visitscotland.brxm.hippobeans.BaseDocument;
 import com.visitscotland.brxm.hippobeans.DevModule;
 import com.visitscotland.brxm.hippobeans.SimpleDevModule;
+import com.visitscotland.brxm.model.ErrorModule;
+import com.visitscotland.brxm.model.Module;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class DevModuleFactory {
 
-    public SimpleDevModule getModule(DevModule document){
-        return new SimpleDevModule(document);
+    private final ComparisonMapper comparisonMapper;
+    private final ResultMapper resultMapper;
+
+    public DevModuleFactory(ComparisonMapper comparisonMapper, ResultMapper resultMapper) {
+        this.comparisonMapper = comparisonMapper;
+        this.resultMapper = resultMapper;
+    }
+
+    public Module<? extends BaseDocument> getModule(DevModule document){
+        if (document.getBespoken() == null) {
+            return new SimpleDevModule(document);
+        }
+        switch (document.getBespoken()){
+            case "obs-form":
+                return comparisonMapper.map(document);
+            case "obs-results":
+                return resultMapper.map();
+            default:
+                return new ErrorModule(document, "The implementation of this module is not ready");
+        }
     }
 }
