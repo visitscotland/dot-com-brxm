0345b6823df753c144d6257d5f775273afbfbf5e
jose.calcines <jose.calcines@visitscotland.com>
2025-07-28 11:46:46 +0100

    DS-1433 - Create constants and add Javadocs

--- site/components/src/main/java/com/visitscotland/brxm/comparator/ComparatorMapper.java
+++ site/components/src/main/java/com/visitscotland/brxm/comparator/ComparatorMapper.java
@@ -20,10 +20,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Mapper service for converting Hippo CMS documents into comparator module data structures.
+ * Handles the mapping of features and providers for the comparator functionality.
+ */
 @Component
 public class ComparatorMapper {
 
     private static final Logger logger = LoggerFactory.getLogger(ComparatorMapper.class);
+    public static final String BSH_OBS_FEATURES = "bsh-obs-features";
+    public static final String BSH_OBS_GROUPS = "bsh-obs-groups";
 
     private final HippoQueryService queryService;
 
@@ -31,13 +37,28 @@ public class ComparatorMapper {
         this.queryService = queryService;
     }
 
+    /**
+     * Maps a DevModule document to a ComparatorModule with features and providers.
+     *
+     * @param document The DevModule document to map
+     * @return ComparatorModule with populated features and providers
+     * @throws VsContractException If contract validation fails
+     * @throws BrxmWrapperException If repository access fails
+     */
     public ComparatorModule map(DevModule document) throws VsContractException, BrxmWrapperException {
         ComparatorModule module = new ComparatorModule(document);
-        module.setFeatures(getFunctions());
+        module.setFeatures(getFeatures());
         module.setProviders(getProviders());
         return module;
     }
 
+    /**
+     * Retrieves all system providers for the comparator.
+     *
+     * @return List of Provider objects
+     * @throws BrxmWrapperException If repository access fails
+     * @throws VsContractException If contract validation fails
+     */
     List<Provider> getProviders() throws BrxmWrapperException, VsContractException {
         List<Provider> entries = new ArrayList<>();
         try {
@@ -55,32 +76,49 @@ public class ComparatorMapper {
         }
     }
 
-    List<Feature> getFunctions() throws BrxmWrapperException, VsContractException {
+    /**
+     * Retrieves all features for the comparator.</br>
+     *
+     * This features depend on document types to be created if they are not they will still appear on the list
+     * but with less information.
+     *
+     * @return List of Feature objects
+     * @throws BrxmWrapperException If repository access fails
+     * @throws VsContractException If contract validation fails
+     */
+    List<Feature> getFeatures() throws BrxmWrapperException, VsContractException {
         List<Feature> entries = new ArrayList<>();
         Map<String, String> valueList = VsComponentManager.get(
-                HippoUtilsService.class).getValueMap("bsh-obs-features");
-        Map<String, Feature> functions = getFunctionsMap();
+                HippoUtilsService.class).getValueMap(BSH_OBS_FEATURES);
+        Map<String, Feature> features = getFeaturesMap();
 
         for (Map.Entry<String, String> entry : valueList.entrySet()) {
-            if (functions.containsKey(entry.getKey())) {
-                entries.add(functions.get(entry.getKey()));
+            if (features.containsKey(entry.getKey())) {
+                entries.add(features.get(entry.getKey()));
             } else {
                 entries.add(new Feature(entry.getKey(), entry.getValue()));
-                logger.warn("Function {} is not defined", entry.getKey());
+                logger.warn("Feature {} is not defined", entry.getKey());
             }
         }
 
         return entries;
     }
 
-    Map<String, Feature> getFunctionsMap() throws BrxmWrapperException, VsContractException {
+    /**
+     * Creates a map of feature IDs to Feature objects from repository data.
+     *
+     * @return Map with feature IDs as keys and Feature objects as values
+     * @throws BrxmWrapperException If repository access fails
+     * @throws VsContractException If contract validation fails
+     */
+    Map<String, Feature> getFeaturesMap() throws BrxmWrapperException, VsContractException {
         Map<String, Feature> map = new HashMap<>();
 
         Map<String, String> featureList = VsComponentManager.get(
-                HippoUtilsService.class).getValueMap("bsh-obs-features");
+                HippoUtilsService.class).getValueMap(BSH_OBS_FEATURES);
 
         Map<String, String> groupList = VsComponentManager.get(
-                HippoUtilsService.class).getValueMap("bsh-obs-groups");
+                HippoUtilsService.class).getValueMap(BSH_OBS_GROUPS);
 
         try {
             HippoBeanIterator iterator = queryService.findAll(ComparatorFeature.class);
