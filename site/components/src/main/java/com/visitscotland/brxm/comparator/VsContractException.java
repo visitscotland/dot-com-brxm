7f6bbe0acf8cd9f25a643febda296df1cb203c92
jose.calcines <jose.calcines@visitscotland.com>
2025-07-30 14:41:18 +0100

    DS-1433 - Documentation added

--- site/components/src/main/java/com/visitscotland/brxm/comparator/VsContractException.java
+++ site/components/src/main/java/com/visitscotland/brxm/comparator/VsContractException.java
@@ -1,5 +1,13 @@
 package com.visitscotland.brxm.comparator;
 
+/**
+ * This exception represent a broken contract between developers and flags that the method is not designed to accept
+ * that value
+ *
+ * This usually happens when values passed are expected to be sanitized or the values to match certain characteristics.
+ *
+ * When this Exception happens
+ */
 public class VsContractException extends Exception {
 
     public VsContractException(String message) {
