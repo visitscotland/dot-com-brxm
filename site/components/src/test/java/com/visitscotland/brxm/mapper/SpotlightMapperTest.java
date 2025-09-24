3979f4d154d717c73cb06cda430b2133c8eca854
jose.calcines <jose.calcines@visitscotland.com>
2025-09-24 13:00:05 +0100

    DS-1658 - Break down unit tests into Newsletter and Spotlight

--- site/components/src/test/java/com/visitscotland/brxm/mapper/SpotlightMapperTest.java
+++ site/components/src/test/java/com/visitscotland/brxm/mapper/SpotlightMapperTest.java
@@ -1,16 +1,18 @@
-package com.visitscotland.brxm.factory;
+package com.visitscotland.brxm.mapper;
 
+import com.visitscotland.brxm.factory.NewsletterFactory;
 import com.visitscotland.brxm.hippobeans.CTABanner;
 import com.visitscotland.brxm.hippobeans.Image;
 import com.visitscotland.brxm.hippobeans.SharedLink;
-import com.visitscotland.brxm.model.Module;
 import com.visitscotland.brxm.model.*;
+import com.visitscotland.brxm.model.Module;
 import com.visitscotland.brxm.services.HippoUtilsService;
 import com.visitscotland.brxm.services.LinkService;
 import com.visitscotland.brxm.services.ResourceBundleService;
 import com.visitscotland.brxm.utils.AnchorFormatter;
 import com.visitscotland.brxm.utils.ContentLogger;
 import com.visitscotland.brxm.utils.SiteProperties;
+import com.visitscotland.brxm.utils.pagebuilder.InvalidContentException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,7 +26,7 @@ import java.util.Locale;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class SignpostFactoryTest {
+class SpotlightMapperTest {
 
     private static final String BUNDLE_ID = "newsletter-signpost";
 
@@ -42,32 +44,11 @@ class SignpostFactoryTest {
     ContentLogger contentLogger;
 
     @InjectMocks
-    SignpostFactory signpostFactory;
-
-    @DisplayName("Newsletter signpost module includes newsletter information from properties")
-    @Test
-    void newsletterSignpost() {
-        when(bundle.getResourceBundle(BUNDLE_ID, "newsletter.cta.text", Locale.UK)).thenReturn("cta.text");
-        when(bundle.getResourceBundle(BUNDLE_ID, "newsletter.cta.link", Locale.UK)).thenReturn("dummy-url");
-        when(bundle.getResourceBundle(BUNDLE_ID, "newsletter.title", Locale.UK)).thenReturn("title");
-        when(bundle.getResourceBundle(BUNDLE_ID, "newsletter.copy", Locale.UK)).thenReturn("copy");
-        when(bundle.getResourceBundle(BUNDLE_ID, "newsletter.image", Locale.UK)).thenReturn("image.jpeg");
-
-        when(utils.createUrlFromNode(any(), anyBoolean())).thenReturn("cta.link");
-
-        SignpostModule module = signpostFactory.createNewsletterSignpostModule(Locale.UK).orElseThrow();
-
-        Assertions.assertEquals("cta.text", module.getCta().getLabel());
-        Assertions.assertEquals("cta.link", module.getCta().getLink());
-        Assertions.assertEquals(LinkType.INTERNAL, module.getCta().getType());
-        Assertions.assertEquals("title", module.getTitle());
-        Assertions.assertEquals("copy", module.getCopy().getContent());
-        Assertions.assertEquals("image.jpeg", module.getImage().getExternalImage());
-    }
+    SpotlightMapper spotlightMapper;
 
     @Test
     @DisplayName("Should return ErrorModule when CTA link is not available")
-    void when_ctaLinkIsNull_then_ErrorModuleIsReturned() {
+    void when_ctaLinkIsNull_then_ErrorModuleIsReturned(){
         CTABanner ctaBanner = mock(CTABanner.class, RETURNS_DEEP_STUBS);
         FlatLink cta = new FlatLink(); // no link set
 
@@ -75,16 +56,13 @@ class SignpostFactoryTest {
         when(ctaBanner.getPath()).thenReturn("path");
         when(linkService.createSimpleLink(any(), any(), any())).thenReturn(cta);
 
-        Module<?> result = signpostFactory.createModule(ctaBanner);
-        Assertions.assertInstanceOf(ErrorModule.class, result);
-
-        verify(contentLogger).warn(any());
+        Assertions.assertThrows(InvalidContentException.class, () ->spotlightMapper.createModule(ctaBanner));
     }
 
 
     @Test
     @DisplayName("Should set label when label is not empty")
-    void when_overrideLabelIsProvided_then_thatLabelShouldBeUsed() {
+    void when_overrideLabelIsProvided_then_thatLabelShouldBeUsed() throws InvalidContentException {
         CTABanner ctaBanner = mock(CTABanner.class, RETURNS_DEEP_STUBS);
         SharedLink linkable = mock(SharedLink.class);
 
@@ -96,7 +74,7 @@ class SignpostFactoryTest {
 
         when(ctaBanner.getCtaLink().getLabel()).thenReturn("Read More");
 
-        Module<?> result = signpostFactory.createModule(ctaBanner);
+        Module<?> result = spotlightMapper.createModule(ctaBanner);
         Assertions.assertInstanceOf(SignpostModule.class, result);
 
         Assertions.assertEquals("Read More", ((SignpostModule) result).getCta().getLabel());
@@ -104,7 +82,7 @@ class SignpostFactoryTest {
 
     @Test
     @DisplayName("Should set image using CTA banner image")
-    void when_imageIsProvided_then_thatImageShouldBeUsed() {
+    void when_imageIsProvided_then_thatImageShouldBeUsed() throws InvalidContentException {
         CTABanner ctaBanner = mock(CTABanner.class, RETURNS_DEEP_STUBS);
         SharedLink linkable = mock(SharedLink.class);
 
@@ -117,7 +95,7 @@ class SignpostFactoryTest {
 
         when(ctaBanner.getImage()).thenReturn(mock(Image.class));
 
-        Module<?> result = signpostFactory.createModule(ctaBanner);
+        Module<?> result = spotlightMapper.createModule(ctaBanner);
         Assertions.assertInstanceOf(SignpostModule.class, result);
         SignpostModule signpostModule = (SignpostModule) result;
 
