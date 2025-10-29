b1ada35549895d6b5dac34efc911ce89f8cfe2c8
stacey.welsh <stacey.welsh@visitscotland.com>
2025-10-29 11:13:17 +0000

    VS-823 (spotlight) content modelling for the spotlight module

---
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/Spotlight.java
@@ -0,0 +1,41 @@
+package com.visitscotland.brxm.hippobeans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import com.visitscotland.brxm.hippobeans.Image;
+import com.visitscotland.brxm.hippobeans.CMSLink;
+
+@HippoEssentialsGenerated(internalName = "visitscotland:Spotlight")
+@Node(jcrType = "visitscotland:Spotlight")
+public class Spotlight extends BaseDocument {
+    @HippoEssentialsGenerated(internalName = "visitscotland:title")
+    public String getTitle() {
+        return getSingleProperty("visitscotland:title");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:anchor")
+    public String getAnchor() {
+        return getSingleProperty("visitscotland:anchor");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:copy")
+    public HippoHtml getCopy() {
+        return getHippoHtml("visitscotland:copy");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:image")
+    public Image getImage() {
+        return getLinkedBean("visitscotland:image", Image.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:ctaLink")
+    public CMSLink getCtaLink() {
+        return getBean("visitscotland:ctaLink", CMSLink.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:translation")
+    public String getTranslation() {
+        return getSingleProperty("visitscotland:translation");
+    }
+}
