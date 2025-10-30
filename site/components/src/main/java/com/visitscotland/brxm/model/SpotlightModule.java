2c2b7e17dbf03e6e5637eea39f6896da60b05fda
stacey.welsh <stacey.welsh@visitscotland.com>
2025-10-30 15:53:18 +0000

    VS-823 (spotlight) implementation for the spotlight module

---
+++ site/components/src/main/java/com/visitscotland/brxm/model/SpotlightModule.java
@@ -0,0 +1,48 @@
+package com.visitscotland.brxm.model;
+
+import com.visitscotland.brxm.hippobeans.Spotlight;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+
+public class SpotlightModule extends Module<Spotlight> {
+
+        private String title;
+        private HippoHtml copy;
+        private FlatLink cta;
+        private FlatImage image;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public HippoHtml getCopy() {
+            return copy;
+        }
+
+        public void setCopy(HippoHtml copy) {
+            this.copy = copy;
+        }
+
+        public FlatLink getCta() {
+            return cta;
+        }
+
+        public void setCta(FlatLink cta) {
+            this.cta = cta;
+        }
+
+        public FlatImage getImage() {
+            return image;
+        }
+
+        public void setImage(FlatImage image) {
+            this.image = image;
+        }
+
+
+}
+
+
