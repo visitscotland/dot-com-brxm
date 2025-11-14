154b6009c5d1a7f8758831a59741f60888851aef
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2025-11-14 09:44:03 +0000

    FEAT VS-852 (Map) Rename map key

--- site/components/src/main/java/com/visitscotland/brxm/utils/SiteProperties.java
+++ site/components/src/main/java/com/visitscotland/brxm/utils/SiteProperties.java
@@ -45,9 +45,10 @@ public class SiteProperties extends Properties {
     static final String ENABLE_IKNOW_MODULE = "iknow-module.enabled";
     static final String EVENTS_LISTINGS_PAGE_SIZE = "events-listings.page-size";
 
-    static final String GOOGLE_MAPS_API = "google.maps-api";
+    static final String GOOGLE_MAPS_API_KEY = "google.maps-api-key";
 
-    //GTM Properties
+    //GTM Propertie
+
     public static final String GTM_CONTAINER_ID = "gtm.container-id";
     public static final String GTM_IS_PRODUCTION = "gtm.is-production";
     public static final String GTM_PREVIEW_QUERY_STRING = "gtm.preview-query-string";
@@ -109,7 +110,7 @@ public class SiteProperties extends Properties {
         return readString(PATH_GLOBAL_SEARCH);
     }
     public String getGoogleMapsApi() {
-        return readString(GOOGLE_MAPS_API);
+        return readString(GOOGLE_MAPS_API_KEY);
     }
 
     public String getSiteNewsletter() {
