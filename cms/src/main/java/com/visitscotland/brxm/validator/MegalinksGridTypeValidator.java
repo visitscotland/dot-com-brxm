8e392965580ccd6fc768381774717895450998e7
stacey.welsh <stacey.welsh@visitscotland.com>
2025-11-19 13:14:58 +0000

    DS-1770 create validator for Megalinks grids

---
+++ cms/src/main/java/com/visitscotland/brxm/validator/MegalinksGridTypeValidator.java
@@ -0,0 +1,100 @@
+package com.visitscotland.brxm.validator;
+
+import com.visitscotland.brxm.translation.SessionFactory;
+import org.onehippo.cms.services.validation.api.ValidationContext;
+import org.onehippo.cms.services.validation.api.Validator;
+import org.onehippo.cms.services.validation.api.Violation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.PathNotFoundException;
+import java.util.Optional;
+
+/**
+ * jcrType = visitscotland:megalinks-grid-type-validator
+ */
+public class MegalinksGridTypeValidator implements Validator<Node> {
+
+    private SessionFactory sessionFactory;
+
+    private static final String LAYOUT_PROPERTY = "visitscotland:layout";
+
+    private static final String GRID = "Grid";
+    private static final String GRID_3 = "Grid 3";
+    private static final String GRID_4 = "Grid 4";
+
+    private static final String EMPTY_LINKS = "emptyLinks";
+    private static final String EXCEPTION = "exception";
+    private static final String NUMBER_OF_LINKS = "numberOfLinks";
+    private static final String NUMBER_OF_LINKS_34 = "numberOfLinks34";
+    private static final String NULL_LAYOUT = "nullLayout";
+    private static final String TRANSLATION = "translation";
+
+    private static final Logger logger = LoggerFactory.getLogger(MegalinksGridTypeValidator.class);
+
+    public MegalinksGridTypeValidator(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public MegalinksGridTypeValidator() {}
+
+    @Override
+    public Optional<Violation> validate(final ValidationContext context, final Node node) {
+
+        try {
+            final String layout = node.hasProperty(LAYOUT_PROPERTY) ? node.getProperty(LAYOUT_PROPERTY).getString() : null;
+
+            if (layout == null) {
+                return Optional.of(context.createViolation(NULL_LAYOUT));
+            }
+
+            if (layout.equals(GRID)) {
+                return validateGrid(context, node);
+            } else if (layout.equals(GRID_3) || layout.equals(GRID_4)) {
+                // to be removed when grid 3 and 4 is deprecated
+                return validateGrid34(context, node);
+            } else {
+                // we can ignore any other layout types
+                return Optional.empty();
+            }
+
+        } catch (PathNotFoundException exception) {
+            exceptionLogger(exception);
+            return Optional.of(context.createViolation(TRANSLATION));
+        } catch (RepositoryException exception) {
+            exceptionLogger(exception);
+            return Optional.of(context.createViolation(EXCEPTION));
+        }
+    }
+
+    // normal grid links - can hold up to 4
+    private Optional<Violation> validateGrid(final ValidationContext context, final Node node) throws RepositoryException {
+
+        final Long nodeCount = node.getNodes().getSize();
+        if (nodeCount == null || nodeCount < 1 ) {
+            return Optional.of(context.createViolation(EMPTY_LINKS));
+        } else if (nodeCount > 4) {
+            return Optional.of(context.createViolation(NUMBER_OF_LINKS));
+        }
+        return Optional.empty();
+
+    }
+
+    // to be removed when grid 3 and 4 is deprecated
+    private Optional<Violation> validateGrid34(final ValidationContext context, final Node node) throws RepositoryException {
+
+        final Long nodeCount = node.getNodes().getSize();
+        if (nodeCount == null || nodeCount < 1 ) {
+            return Optional.of(context.createViolation(EMPTY_LINKS));
+        } else if (nodeCount < 3 || nodeCount > 4) {
+            return Optional.of(context.createViolation(NUMBER_OF_LINKS_34));
+        }
+        return Optional.empty();
+    }
+
+    private void exceptionLogger(Exception exception) {
+        logger.error(exception.getMessage(), exception);
+    }
+}
