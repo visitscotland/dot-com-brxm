21ed42df1e0e45c2bf9e8ed930a1a4c385c7bbd2
Juan Luis Rodriguez Hurtado <Juanluis.hurtado@visitscotland.com>
2026-01-14 16:27:55 +0000

    FEAT VS-802 (DMS) remove whats nearby links from itineraries

--- site/components/src/main/java/com/visitscotland/brxm/mapper/page/ItineraryMapper.java
+++ site/components/src/main/java/com/visitscotland/brxm/mapper/page/ItineraryMapper.java
@@ -108,8 +108,6 @@ public class ItineraryMapper {
         page.setDistance(calculateDistance ? totalDistance.setScale(0, RoundingMode.HALF_UP) :BigDecimal.valueOf(itinerary.getDistance()));
 
         populateFirstAndLastStopTexts(page, firstStop, lastStop);
-        /* VS-802 DMS retire tasks, remove whats nearby links to PSR results*/
-        //populateLastStopLinks(page, lastStop, locale);
         populateTransports(page, itinerary.getTransports());
         populateThemes(page, itinerary.getTheme());
         populateAreas(page, itinerary.getAreas());
@@ -379,17 +377,6 @@ public class ItineraryMapper {
             return entries;
         }
         return Collections.emptyList();
-    }
-
-    @Deprecated
-    private void populateLastStopLinks(ItineraryPage page, ItineraryStopModule lastStop, Locale locale) {
-        if (lastStop != null && lastStop.getCoordinates() != null) {
-            var latitude = lastStop.getCoordinates().getLatitude();
-            var longitude = lastStop.getCoordinates().getLongitude();
-
-            page.setLastStopNearbyEat(getProductSearchUrl(locale, "cate", latitude, longitude));
-            page.setLastStopNearbyStay(getProductSearchUrl(locale, "acco", latitude, longitude));
-        }
     }
 
     @Deprecated
