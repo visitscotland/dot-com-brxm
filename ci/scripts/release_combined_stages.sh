97e3c67ca8b5a3c8bd724caf467edf7f988b6be2
Matthew Syrigos <matthew.syrigos@visitscotland.com>
2026-01-26 11:43:49 +0000

    NON-JIRA MAINT: added debugging info regarding email subject file

--- ci/scripts/release_combined_stages.sh
+++ ci/scripts/release_combined_stages.sh
@@ -373,7 +373,8 @@ step_5_compose_email() {
   EMAIL_SUBJECT_FILE="$OUT_DIR/email.subject.txt"
   # Write subject as a single line
   printf '%s' "$EMAIL_SUBJECT" > "$EMAIL_SUBJECT_FILE"
-  echo "            INFO: EMAIL_SUBJECT=$EMAIL_SUBJECT"
+  echo "            INFO: Wrote subject: $EMAIL_SUBJECT_FILE"
+  echo "            INFO: EMAIL_SUBJECT: $EMAIL_SUBJECT"
 
   EMAIL_HTML_FILE="$OUT_DIR/email.html"
 
