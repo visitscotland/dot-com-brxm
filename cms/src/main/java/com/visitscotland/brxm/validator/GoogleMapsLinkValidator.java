684b39e05e951e940d42b6bfa69d425591d9e192
stacey.welsh <stacey.welsh@visitscotland.com>
2026-01-26 13:56:54 +0000

    VS-1091 add .co.uk links to google maps validator regex

--- cms/src/main/java/com/visitscotland/brxm/validator/GoogleMapsLinkValidator.java
+++ cms/src/main/java/com/visitscotland/brxm/validator/GoogleMapsLinkValidator.java
@@ -19,7 +19,7 @@ public class GoogleMapsLinkValidator implements Validator<Node>  {
 
     // regex to extract coordinates from url using @ coordinates
     private static final String URL_REGEX =
-            "(?i)https://www\\.google\\.com/maps/place/[^/]*/@(-?\\d{1,3}\\.\\d{1,20}),(-?\\d{1,3}\\.\\d{1,20}),.*";
+            "(?i)https://www\\.google\\.(com|co\\.uk)?/maps/place/[^/]*/@(-?\\d{1,3}\\.\\d{1,20}),(-?\\d{1,3}\\.\\d{1,20}),.*";
 
     static final String MAP_LINK = "visitscotland:mapLink";
     static final String LINK_FIELD = "visitscotland:link";
@@ -50,7 +50,7 @@ public class GoogleMapsLinkValidator implements Validator<Node>  {
                 return Optional.of(context.createViolation(ERROR));
             }
         } catch (RepositoryException e) {
-            logger.error("A repository exception occurred. {}", e);
+            logger.error("A repository exception occurred.", e);
             return Optional.of(context.createViolation(EXCEPTION));
         }
     }
