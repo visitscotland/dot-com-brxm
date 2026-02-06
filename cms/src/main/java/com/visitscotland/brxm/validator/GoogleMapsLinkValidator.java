cbe4c2ba37472951c27ac6519f2d1b6484cc7da2
stacey.welsh <stacey.welsh@visitscotland.com>
2026-02-06 14:10:24 +0000

    VS-1091 modify google-maps-link-validator to check for basic Google Maps urls in Itineraries

--- cms/src/main/java/com/visitscotland/brxm/validator/GoogleMapsLinkValidator.java
+++ cms/src/main/java/com/visitscotland/brxm/validator/GoogleMapsLinkValidator.java
@@ -18,13 +18,13 @@ public class GoogleMapsLinkValidator implements Validator<Node>  {
     private static final Logger LOGGER = LoggerFactory.getLogger(GoogleMapsLinkValidator.class);
 
     /**
-     *     regex to extract coordinates from google maps place url using @ coordinates (.com and .co.uk)
+     *     regex to extract coordinates from Google Maps place url using @ coordinates (.com and .co.uk)
      */
     private static final String URL_REGEX =
             "(?i)https://www\\.google\\.(com|co\\.uk)?/maps/place/[^/]*/@(-?\\d{1,3}\\.\\d{1,20}),(-?\\d{1,3}\\.\\d{1,20}),.*";
 
     /**
-     *     simple regex to check for google urls (.com and .co.uk)
+     *     simple regex to check for Google urls (.com and .co.uk)
      */
     private static final String BASIC_URL_REGEX = "(?i)https://www\\.google\\.(com|co\\.uk)?/maps/.*";
 
@@ -69,6 +69,12 @@ public class GoogleMapsLinkValidator implements Validator<Node>  {
         }
     }
 
+    /**
+     * Validate mapLink from Days
+     * @param context
+     * @param mapLink
+     * @return
+     */
     private Optional<Violation> validateDay(final ValidationContext context, final String mapLink) {
         if (mapLink != null && mapLink.matches(URL_REGEX)) {
             return Optional.empty();
@@ -77,6 +83,12 @@ public class GoogleMapsLinkValidator implements Validator<Node>  {
         }
     }
 
+    /**
+     * Validate mapLink from Itineraries
+     * @param context
+     * @param node
+     * @return
+     */
     private Optional<Violation> validateItinerary(final ValidationContext context, final Node node) {
         try {
             final String mapLink = node.getProperty(LINK_FIELD).getString();
