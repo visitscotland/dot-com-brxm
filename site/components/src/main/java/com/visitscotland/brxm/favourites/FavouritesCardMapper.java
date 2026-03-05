9f91355b7f5b0ceb9381b6cc61e51e5b1f1288dc
stacey.welsh <stacey.welsh@visitscotland.com>
2026-03-05 14:32:12 +0000

    DS-1881 allow fully qualified urls

--- site/components/src/main/java/com/visitscotland/brxm/favourites/FavouritesCardMapper.java
+++ site/components/src/main/java/com/visitscotland/brxm/favourites/FavouritesCardMapper.java
@@ -4,11 +4,22 @@ import com.google.common.collect.ImmutableList;
 import com.visitscotland.brxm.components.content.GeneralContentComponent;
 import com.visitscotland.brxm.hippobeans.General;
 import com.visitscotland.brxm.hippobeans.Page;
+import com.visitscotland.brxm.mapper.ImageMapper;
+import com.visitscotland.brxm.model.FlatImage;
+import com.visitscotland.brxm.model.FlatLink;
+import com.visitscotland.brxm.model.Module;
 import com.visitscotland.brxm.model.favourites.FavouritesCard;
+import com.visitscotland.brxm.services.LinkService;
+import org.hippoecm.hst.configuration.hosting.Mount;
+import org.hippoecm.hst.container.RequestContextProvider;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.core.request.HstRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.RequestContext;
+
+import java.util.Locale;
 
 /**
  * Mapper class to create favourites cards from supported page info
@@ -18,12 +29,20 @@ public class FavouritesCardMapper {
 
     private static final Logger logger = LoggerFactory.getLogger(FavouritesCardMapper.class);
 
+    private final LinkService linkService;
+    private final ImageMapper imageMapper;
+
     // content types can be added here as they are supported (see also PageAssembler)
     private static final ImmutableList<String> FAVOURITABLE_CONTENT = ImmutableList.<String>builder()
             .add("visitscotland:Itinerary")
             .add("visitscotland:Destination")
             .add("visitscotland:Listicle")
             .build();
+
+    public FavouritesCardMapper(LinkService linkService, ImageMapper imageMapper) {
+        this.linkService = linkService;
+        this.imageMapper = imageMapper;
+    }
 
     public FavouritesCard getFavouritesCard(HippoBean bean) {
 
@@ -54,15 +73,33 @@ public class FavouritesCardMapper {
             return null;
         }
 
+        final Locale locale = page.getLocale();
+
+        final String pageUrl = linkService.getPlainLink(page.getLocale(), page, null);
+
+        final FlatImage image = imageMapper.createImage(page.getHeroImage(), new Module<>(), locale);
+
         FavouritesCard card = new FavouritesCard();
         card.setUuid(page.getCanonicalHandleUUID());
         card.setTitle(page.getTitle());
         card.setTeaser(page.getTeaser());
-        card.setImage(page.getHeroImage().getCanonicalPath());
-        card.setUrl(page.getPath()); // not working
+        card.setImage(toURL(page.getHeroImage()));
+        card.setUrl(toURL(page)); // not working
 
         return card;
 
     }
+
+    private String toURL(HippoBean document){
+        final boolean FULLY_QUALIFIED = true;
+        HstRequestContext context = RequestContextProvider.get();
+        Mount mount = context.getMount("vs-en");
+//
+//        FlatImage image = imageMapper.createImage(page.getHeroImage(), null, null);
+
+        return context.getHstLinkCreator().create(document.getNode(), mount)
+                .toUrlForm(context, FULLY_QUALIFIED);
+    }
+
 }
 
