6a6f00291ccd702dac3e66144a71e7fff6a1ed8c
jose.calcines <jose.calcines@visitscotland.com>
2026-03-24 17:03:14 +0000

    VS-1414 - Generate Beans

---
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/ArticleStyled.java
@@ -0,0 +1,13 @@
+package com.visitscotland.brxm.hippobeans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+
+@HippoEssentialsGenerated(internalName = "visitscotland:ArticleStyled")
+@Node(jcrType = "visitscotland:ArticleStyled")
+public class ArticleStyled extends Article {
+    @HippoEssentialsGenerated(internalName = "visitscotland:theme")
+    public String getTheme() {
+        return getSingleProperty("visitscotland:theme");
+    }
+}
---
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/ArticleStyledSectionCOM.java
@@ -0,0 +1,20 @@
+package com.visitscotland.brxm.hippobeans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoCompound;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+
+@HippoEssentialsGenerated(internalName = "visitscotland:ArticleStyledSectionCOM")
+@Node(jcrType = "visitscotland:ArticleStyledSectionCOM")
+public class ArticleStyledSectionCOM extends HippoCompound {
+    @HippoEssentialsGenerated(internalName = "visitscotland:heading")
+    public String getHeading() {
+        return getSingleProperty("visitscotland:heading");
+    }
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:copy")
+    public HippoHtml getCopy() {
+        return getHippoHtml("visitscotland:copy");
+    }
+}
