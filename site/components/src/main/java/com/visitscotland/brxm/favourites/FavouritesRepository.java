c54ddc5d95acd93b3d312ae6d59cddd7ea8b443b
jose.calcines <jose.calcines@visitscotland.com>
2026-04-01 17:34:41 +0100

    VS-1428 - Change error handling to exception to prevent multiples messages from a single issue

--- site/components/src/main/java/com/visitscotland/brxm/favourites/FavouritesRepository.java
+++ site/components/src/main/java/com/visitscotland/brxm/favourites/FavouritesRepository.java
@@ -2,7 +2,7 @@ package com.visitscotland.brxm.favourites;
 
 import com.visitscotland.brxm.model.favourites.FavouritesCard;
 import com.visitscotland.brxm.services.HippoUtilsService;
-import com.visitscotland.brxm.utils.SiteProperties;
+import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -23,12 +23,10 @@ public class FavouritesRepository {
     private static final String UUID_REGEX = HEX_REGEX + "{8}-" + HEX_REGEX + "{4}-"+ HEX_REGEX + "{4}-" + HEX_REGEX + "{4}-" + HEX_REGEX + "{12}";
 
     private final FavouritesCardMapper favouritesCardMapper;
-    private final SiteProperties siteProperties;
     private final HippoUtilsService hippoUtilsService;
 
-    public FavouritesRepository(FavouritesCardMapper favouritesCardMapper, SiteProperties siteProperties, HippoUtilsService hippoUtilsService) {
+    public FavouritesRepository(FavouritesCardMapper favouritesCardMapper, HippoUtilsService hippoUtilsService) {
         this.favouritesCardMapper = favouritesCardMapper;
-        this.siteProperties = siteProperties;
         this.hippoUtilsService = hippoUtilsService;
     }
 
@@ -43,12 +41,13 @@ public class FavouritesRepository {
         if (favouritesRequest != null) {
             for (final String uuid : favouritesRequest.getUuids()) {
                 if (uuid != null && uuid.matches(UUID_REGEX)) {
-                    final FavouritesCard card = favouritesCardMapper.getFavouritesCard(hippoUtilsService.getContentByUuid(uuid));
-                    if (card == null) {
-                        logger.info("Unable to get favourites card content for UUID {}", uuid);
-                        failedUuids.add(uuid);
-                    } else {
+                    try {
+                        final HippoBean page = hippoUtilsService.getContentByUuid(uuid);
+                        final FavouritesCard card = favouritesCardMapper.getFavouritesCard(page);
                         cards.add(card);
+                    } catch (FavouritesException e) {
+                        logger.info("Unable to get favourites card content for UUID {}: {}}", uuid, e.getMessage());
+                        failedUuids.add(uuid);
                     }
                } else {
                     logger.info("Invalid uuid provided: {}", uuid);
