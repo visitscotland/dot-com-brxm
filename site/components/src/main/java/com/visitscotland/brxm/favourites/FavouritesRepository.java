6f2913dfa1b7ce05118055b6976acfb9951062ad
jose.calcines <jose.calcines@visitscotland.com>
2026-04-03 08:33:00 +0100

    VS-1428 - Improve null checking

--- site/components/src/main/java/com/visitscotland/brxm/favourites/FavouritesRepository.java
+++ site/components/src/main/java/com/visitscotland/brxm/favourites/FavouritesRepository.java
@@ -38,26 +38,34 @@ public class FavouritesRepository {
         List<String> failedUuids = new ArrayList<>();
         FavouritesCardResponse response = new FavouritesCardResponse();
 
-        if (favouritesRequest != null) {
-            for (final String uuid : favouritesRequest.getUuids()) {
-                if (uuid != null && uuid.matches(UUID_REGEX)) {
-                    try {
-                        final HippoBean page = hippoUtilsService.getContentByUuid(uuid);
-                        final FavouritesCard card = favouritesCardMapper.getFavouritesCard(page);
-                        cards.add(card);
-                    } catch (FavouritesException e) {
-                        logger.info("Unable to get favourites card content for UUID {}: {}}", uuid, e.getMessage());
-                        failedUuids.add(uuid);
-                    }
-               } else {
-                    logger.info("Invalid uuid provided: {}", uuid);
-                    failedUuids.add(uuid);
-                }
-            }
-        } else {
-            logger.info("FavouritesRequest is null.");
+        if (favouritesRequest == null || favouritesRequest.getUuids() == null) {
+            logger.warn("FavouritesRequest or its UUID list cannot be null.");
+            /* TODO: Can we throw an exception here instead?
+             * It would be more consistent with the rest of the codebase to throw an exception for invalid input rather
+             * than returning null.
+             * If we return null, it could lead to NullPointerExceptions further down the line if the caller is
+             * not checking for null. By throwing an IllegalArgumentException, we can provide a clear message about
+             * what went wrong and prevent potential issues later on.
+             */
+            //throw new IllegalArgumentException("FavouritesRequest and its UUID list cannot be null.");
             return null;
         }
+        for (final String uuid : favouritesRequest.getUuids()) {
+            if (uuid != null && uuid.matches(UUID_REGEX)) {
+                try {
+                    final HippoBean page = hippoUtilsService.getContentByUuid(uuid);
+                    final FavouritesCard card = favouritesCardMapper.getFavouritesCard(page);
+                    cards.add(card);
+                } catch (FavouritesException e) {
+                    logger.info("Unable to get favourites card content for UUID {}: {}}", uuid, e.getMessage());
+                    failedUuids.add(uuid);
+                }
+            } else {
+                logger.info("Invalid uuid provided: {}", uuid);
+                failedUuids.add(uuid);
+            }
+        }
+
         response.setCards(cards);
         response.setFailedUuids(failedUuids);
         return response;
