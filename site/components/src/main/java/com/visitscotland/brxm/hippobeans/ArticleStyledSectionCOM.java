6e9787efed7679390f85ba749bd47952f79d9fe7
jose.calcines <jose.calcines@visitscotland.com>
2026-04-14 16:58:35 +0100

    DS-1931 - Generated Beans

--- site/components/src/main/java/com/visitscotland/brxm/hippobeans/ArticleStyledSectionCOM.java
+++ site/components/src/main/java/com/visitscotland/brxm/hippobeans/ArticleStyledSectionCOM.java
@@ -2,10 +2,13 @@ package com.visitscotland.brxm.hippobeans;
 
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoCompound;
-import org.hippoecm.hst.content.beans.standard.HippoHtml;
 
 @HippoEssentialsGenerated(internalName = "visitscotland:ArticleStyledSectionCOM")
 @Node(jcrType = "visitscotland:ArticleStyledSectionCOM")
 public class ArticleStyledSectionCOM extends ArticleSection {
+
+    @HippoEssentialsGenerated(internalName = "visitscotland:heading")
+    public String getHeading() {
+        return getSingleProperty("visitscotland:heading");
+    }
 }
