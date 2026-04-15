f82e82fabb03e3206dcda6908bec26a8d1554fb1
Jose Calcines <46224849+jcalcines@users.noreply.github.com>
2026-04-15 09:00:03 +0100

    Apply suggestion from @coderabbitai[bot]
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- site/components/src/main/java/com/visitscotland/brxm/components/content/PageContentComponent.java
+++ site/components/src/main/java/com/visitscotland/brxm/components/content/PageContentComponent.java
@@ -259,7 +259,7 @@ public class PageContentComponent<T extends Page> extends ContentComponent {
         VideoLink videoDocument = getDocument(request).getHeroVideo();
         if (videoDocument != null && videoDocument.getVideoLink() != null) {
             EnhancedLink video = linksService.createVideo(videoDocument.getVideoLink(), introModule, request.getLocale());
-            if (Contract.isEmpty((video.getYoutubeId()))) {
+            if (Contract.isEmpty(video.getYoutubeId())) {
                 request.setModel(VIDEO_HEADER, true);
                 pageConfig.addProperty(HERO_AMBIENT_VIDEO, true);
                 pageConfig.addAllSiteLabels(AMBIENT_VIDEO_BUNDLE);
