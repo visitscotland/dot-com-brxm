7900e4e948e9119c64be2ace4e74b03c0c7128dc
jose.calcines <jose.calcines@visitscotland.com>
2026-05-08 14:05:39 +0100

    VS-1344 - Fix unit tests

--- site/components/src/test/java/com/visitscotland/brxm/pagebuilder/page/GeneralPageAdapterTest.java
+++ site/components/src/test/java/com/visitscotland/brxm/pagebuilder/page/GeneralPageAdapterTest.java
@@ -1,14 +1,13 @@
 package com.visitscotland.brxm.pagebuilder.page;
 
 import com.visitscotland.brxm.hippobeans.General;
-import com.visitscotland.brxm.hippobeans.Page;
 import com.visitscotland.brxm.hippobeans.PageLinksSectionCompound;
 import com.visitscotland.brxm.mapper.page.CategoryCardsMapper;
-import com.visitscotland.brxm.model.megalinks.EnhancedLink;
 import com.visitscotland.brxm.model.megalinks.LinksModule;
 import com.visitscotland.brxm.pagebuilder.PageCompositionException;
 import com.visitscotland.brxm.pagebuilder.PageCompositionHelper;
 import com.visitscotland.brxm.pagebuilder.model.PageIntro;
+import com.visitscotland.brxm.pagebuilder.page.adapter.GeneralPageAdapter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,7 +35,6 @@ class GeneralPageAdapterTest {
         pageTemplateInitializer = mock(PageTemplateInitializer.class);
         adapter = new GeneralPageAdapter(categoryCardsMapper, pageTemplateInitializer);
         pageConfig = mock(PageCompositionHelper.class);
-        when(pageConfig.getLocale()).thenReturn(LOCALE);
     }
 
     @Test
@@ -45,12 +43,14 @@ class GeneralPageAdapterTest {
         General page = mock(General.class);
 
         when(pageConfig.getPage()).thenReturn(page);
+        when(pageConfig.getLocale()).thenReturn(LOCALE);
         when(page.getCategoryLinks()).thenReturn(mock(PageLinksSectionCompound.class));
         when(categoryCardsMapper.getCategoryCards(eq(LOCALE), any()))
                 .thenReturn(new LinksModule<>());
+        when(pageTemplateInitializer.getPageIntro(pageConfig)).thenReturn(new PageIntro(page));
 
 
-        PageIntro result = adapter.getPageIntro(pageConfig);
+        PageIntro result = adapter.getPageIntro(pageConfig).orElseThrow();
 
         assertSame(page, result.getHippoBean());
         assertNotNull(result.getCategorySection());
@@ -62,8 +62,9 @@ class GeneralPageAdapterTest {
         General page = mock(General.class);
 
         when(pageConfig.getPage()).thenReturn(page);
+        when(pageTemplateInitializer.getPageIntro(pageConfig)).thenReturn(new PageIntro(page));
 
-        PageIntro result = adapter.getPageIntro(pageConfig);
+        PageIntro result = adapter.getPageIntro(pageConfig).orElseThrow();
 
         assertSame(page, result.getHippoBean());
         assertNull(result.getCategorySection());
