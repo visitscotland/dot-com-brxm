0d304784995c5bc41d362042f4cf0a723df88dfc
jose.calcines <jose.calcines@visitscotland.com>
2026-05-15 16:08:38 +0100

    VS-1555 - Add Newsletters to PageTemplateInitializer

--- site/components/src/main/java/com/visitscotland/brxm/pagebuilder/page/PageTemplateInitializer.java
+++ site/components/src/main/java/com/visitscotland/brxm/pagebuilder/page/PageTemplateInitializer.java
@@ -1,19 +1,29 @@
 package com.visitscotland.brxm.pagebuilder.page;
 
 import com.visitscotland.brxm.components.content.service.AuthorService;
+import com.visitscotland.brxm.factory.NewsletterFactory;
 import com.visitscotland.brxm.hippobeans.Page;
+import com.visitscotland.brxm.model.SignpostModule;
 import com.visitscotland.brxm.pagebuilder.PageCompositionException;
 import com.visitscotland.brxm.pagebuilder.PageCompositionHelper;
 import com.visitscotland.brxm.pagebuilder.model.PageIntro;
+import com.visitscotland.brxm.utils.SiteProperties;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class PageTemplateInitializer  {
 
     private final AuthorService authorService;
+    private final NewsletterFactory newsletterFactory;
+    private final SiteProperties properties;
 
-    public PageTemplateInitializer(AuthorService authorService) {
+    public PageTemplateInitializer(AuthorService authorService, NewsletterFactory newsletterFactory
+            , SiteProperties properties) {
         this.authorService = authorService;
+        this.newsletterFactory = newsletterFactory;
+        this.properties = properties;
     }
 
     public PageIntro getPageIntro(PageCompositionHelper pageCompositionHelper) throws PageCompositionException {
@@ -21,7 +31,22 @@ public class PageTemplateInitializer  {
         final PageIntro pageTemplate = new PageIntro(page);
 
         authorService.getBlog(pageCompositionHelper).ifPresent(pageTemplate::setAuthor);
+        addNewsletterSignup(pageCompositionHelper);
 
         return pageTemplate;
+    }
+
+    /**
+     * Adds the newsletter configuration to the request taking into account the target: (VisitScotland, Business Events or Ski)
+     */
+    protected Optional<SignpostModule> addNewsletterSignup(PageCompositionHelper pageCompositionHelper) throws PageCompositionException {
+        Page page = pageCompositionHelper.getPage();
+        if (Boolean.TRUE.equals(page.getHideNewsletter())) {
+            return Optional.empty();
+        } else if (pageCompositionHelper.getRequestPathInfo().contains(properties.getSiteSkiSection())) {
+            return newsletterFactory.createSnowAlertsModule(pageCompositionHelper.getLocale());
+        } else {
+            return newsletterFactory.createNewsletterSignpostModule(pageCompositionHelper.getLocale());
+        }
     }
 }
