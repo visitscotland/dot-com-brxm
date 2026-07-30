5d4e8c04cfadfb61348eefab662c1752dfa2e408
stacey.welsh <stacey.welsh@visitscotland.com>
2026-07-30 13:00:32 +0100

    VS-1684 remove stops code from itinerary logic and tests

--- site/components/src/main/java/com/visitscotland/brxm/model/ItineraryPage.java
+++ site/components/src/main/java/com/visitscotland/brxm/model/ItineraryPage.java
@@ -6,9 +6,7 @@ import com.visitscotland.brxm.model.megalinks.Entry;
 import com.visitscotland.brxm.pagebuilder.model.PageTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ItineraryPage extends PageTemplate {
 
@@ -19,34 +17,12 @@ public class ItineraryPage extends PageTemplate {
     private List<String> locations;
     private String iframeMap;
     private Entry theme;
-    // Properties to be removed from the delivery API
-    private String firstStopLocation;
-    private String lastStopLocation;
     private List<Day> days;
     private Integer dayCount;
-    private Map<String, ItineraryStopModule> stops;
-    private String lastStopNearbyEat;
-    private String lastStopNearbyStay;
     private FlatLink mapLink;
 
     public ItineraryPage(Page page) {
         super(page);
-    }
-
-    public String getFirstStopLocation() {
-        return firstStopLocation;
-    }
-
-    public void setFirstStopLocation(String firstStopLocation) {
-        this.firstStopLocation = firstStopLocation;
-    }
-
-    public String getLastStopLocation() {
-        return lastStopLocation;
-    }
-
-    public void setLastStopLocation(String lastStopLocation) {
-        this.lastStopLocation = lastStopLocation;
     }
 
     public List<Day> getDays() {
@@ -67,21 +43,6 @@ public class ItineraryPage extends PageTemplate {
 
     public void setDistance(BigDecimal distance) {
         this.distance = distance;
-    }
-
-    public Map<String, ItineraryStopModule> getStops() {
-        return stops;
-    }
-
-    public void setStops(Map<String, ItineraryStopModule> stops) {
-        this.stops = stops;
-    }
-
-    public void addStop(ItineraryStopModule module){
-        if (stops == null){
-            stops = new HashMap<>();
-        }
-        stops.put(module.getIdentifier(), module);
     }
 
     public List<Entry> getTransports() {
@@ -130,22 +91,6 @@ public class ItineraryPage extends PageTemplate {
 
     public void setTheme(Entry theme) {
         this.theme = theme;
-    }
-
-    public String getLastStopNearbyEat() {
-        return lastStopNearbyEat;
-    }
-
-    public void setLastStopNearbyEat(String lastStopNearbyEat) {
-        this.lastStopNearbyEat = lastStopNearbyEat;
-    }
-
-    public String getLastStopNearbyStay() {
-        return lastStopNearbyStay;
-    }
-
-    public void setLastStopNearbyStay(String lastStopNearbyStay) {
-        this.lastStopNearbyStay = lastStopNearbyStay;
     }
 
     public FlatLink getMapLink() { return mapLink; }
